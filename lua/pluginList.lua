3204c63d04b82c85cd96ef58368f90a7355a6835
Florian Krause <florian.krause@aboutyou.com>
2021-06-21 11:58:16 +0200

    First migration

---
+++ lua/pluginList.lua
@@ -0,0 +1,61 @@
+local packer = require("packer")
+local use = packer.use
+
+-- using { } for using different branch , loading plugin with certain commands etc
+return packer.startup(
+    function()
+        use "wbthomason/packer.nvim"
+        use 'famiu/nvim-reload'
+
+        -- color related stuff
+        use "siduck76/nvim-base16.lua"
+        use "norcalli/nvim-colorizer.lua"
+	use 'shaunsingh/nord.nvim'
+
+        -- lang stuff
+        use "nvim-treesitter/nvim-treesitter"
+        use "neovim/nvim-lspconfig"
+        use "hrsh7th/nvim-compe"
+        use "onsails/lspkind-nvim"
+        use "sbdchd/neoformat"
+        use "nvim-lua/plenary.nvim"
+        use "kabouzeid/nvim-lspinstall"
+
+        use "lewis6991/gitsigns.nvim"
+        use "akinsho/nvim-bufferline.lua"
+        use "glepnir/galaxyline.nvim"
+        use "windwp/nvim-autopairs"
+        use "alvan/vim-closetag"
+        use "folke/trouble.nvim"
+
+        -- Comment
+        use "terrortylor/nvim-comment"
+
+        -- snippet support
+        use "hrsh7th/vim-vsnip"
+        use "rafamadriz/friendly-snippets"
+
+        -- file managing , picker etc
+        use "kyazdani42/nvim-tree.lua"
+        use "kyazdani42/nvim-web-devicons"
+        use "nvim-telescope/telescope.nvim"
+        use "nvim-telescope/telescope-media-files.nvim"
+        use "nvim-lua/popup.nvim"
+        use "kdheepak/lazygit.nvim"
+
+
+        -- misc
+        use "glepnir/dashboard-nvim"
+        use "tweekmonster/startuptime.vim"
+        use "907th/vim-auto-save"
+        use "karb94/neoscroll.nvim"
+        use "kdav5758/TrueZen.nvim"
+        use "folke/which-key.nvim"
+        use {"lukas-reineke/indent-blankline.nvim", branch = "lua"}
+    end,
+    {
+        display = {
+            border = {"┌", "─", "┐", "│", "┘", "─", "└", "│"}
+        }
+    }
+)
