34157df53d15e8ca19dbcab1cce05feba5dcad7e
Florian Krause <florian.krause@aboutyou.com>
2021-06-21 16:05:15 +0200

    Fix invalid config

--- lua/multiline.lua
+++ lua/multiline.lua
@@ -1,6 +1,8 @@
 local g = vim.g
-g.VM_maps["Select All"] = '<leader>a' 
-g.VM_maps["Select Cursor Down"] = '<M-C-Down>'
-g.VM_maps["Select Cursor Up"] = '<M-C-Up>'
-g.VM_maps["Visual All"] = '<leader>a' 
-g.VM_maps["Visual Cursors"] = '<C-n>'
+g.VM_maps = {
+  ["Select All"] = '<leader>a', 
+  ["Select Cursor Down"] = '<M-C-Down>',
+  ["Select Cursor Up"] = '<M-C-Up>',
+  ["Visual All"] = '<leader>a',
+  ["Visual Cursors"] = '<C-n>'
+}
