c92853f5cbdc6bd3f1eec3c99726847beedb2172
Florian Krause <florian.krause_ext@zooplus.com>
2023-01-05 14:43:59 +0100

    Simplify setup big time

---
+++ init.lua
@@ -0,0 +1,287 @@
+-------------------------------------------------------------------------------
+-- Options
+-------------------------------------------------------------------------------
+-- globals
+vim.g.mapleader = " "
+
+local scopes = { o = vim.o, b = vim.bo, w = vim.wo }
+
+local function opt(scope, key, value)
+  scopes[scope][key] = value
+  if scope ~= "o" then
+    scopes["o"][key] = value
+  end
+end
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+local opts = {}
+
+-- terminal escape
+map("t", "<esc>", [[<C-\><C-n>]], opts)
+-- Use cursor to select
+map("c", "<down>", 'pumvisible() ? "<c-n>": "<down>"', { noremap = true, expr = true, silent = false })
+map("c", "<up>", 'pumvisible() ? "<c-p>": "<up>"', { noremap = true, expr = true, silent = false })
+
+opt("o", "ruler", false)
+opt("o", "showmode", false)
+opt("o", "hidden", true)
+opt("o", "ignorecase", true)
+opt("o", "splitbelow", true)
+opt("o", "splitright", true)
+opt("o", "termguicolors", true)
+opt("w", "cul", true)
+
+opt("o", "mouse", "a")
+
+opt("w", "signcolumn", "yes")
+opt("o", "cmdheight", 1)
+
+opt("o", "updatetime", 250)
+opt("o", "clipboard", "unnamedplus")
+opt("o", "timeoutlen", 500)
+
+-- Numbers
+opt("w", "number", true)
+opt("o", "numberwidth", 2)
+opt("w", "relativenumber", true)
+
+-- for indenline
+opt("b", "expandtab", true)
+opt("b", "shiftwidth", 2)
+opt("b", "smartindent", true)
+
+opt("o", "updatetime", 250)
+
+vim.cmd("set colorcolumn=80")
+vim.cmd("set laststatus=80")
+
+vim.cmd([[autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4]])
+vim.cmd([[autocmd Filetype yaml setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2]])
+vim.cmd([[autocmd Filetype tf setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2]])
+-- notify file change
+vim.cmd([[autocmd FocusGained * checktime]], false)
+-- Reload nerd tree when coming from lazygit
+-- vim.cmd([[autocmd FocusGained * NvimTreeRefresh]], false)
+
+-------------------------------------------------------------------------------
+-- Bootstrap Package Manager
+-------------------------------------------------------------------------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  print("Installing 'folke/lazy.nvim'...")
+  vim.fn.system({ "git", "clone", "https://github.com/folke/lazy.nvim.git", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-------------------------------------------------------------------------------
+-- Plugins
+-------------------------------------------------------------------------------
+require("lazy").setup {
+  { "shaunsingh/nord.nvim",
+    config = function()
+      vim.opt.termguicolors = true
+      vim.cmd.colorscheme("nord")
+    end },
+  { "nvim-lualine/lualine.nvim",
+    dependencies = "kyazdani42/nvim-web-devicons",
+    config = function()
+      local config = require("lualine-config")
+      require("lualine").setup(config)
+    end
+  },
+  { "karb94/neoscroll.nvim", config = true },
+  { "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons",
+      "MunifTanjim/nui.nvim"
+    },
+    keys = {
+      { "<Leader>n", "<CMD>Neotree toggle<CR>", mode = { "n" } }
+    },
+    config = true },
+  { "ethanholz/nvim-lastplace", config = true },
+  { "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = "nvim-lua/plenary.nvim",
+    keys = {
+      { "<Leader>ff", "<CMD>Telescope find_files<CR>", mode = { "n" } },
+      { "<Leader>fw", "<CMD>Telescope live_grep<CR>", mode = { "n" } },
+      { "<Leader>fb", "<CMD>Telescope buffers<CR>", mode = { "n" } },
+      { "<Leader>f", "<CMD>Telescope oldfiles<CR>", mode = { "n" } },
+      { "<Leader>c", "<CMD>Telescope commands<CR>", mode = { "n" } },
+      { "<Leader>r", "<CMD>Telescope resume<CR>", mode = { "n" } },
+    },
+    config = true },
+  { "williamboman/mason.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "jose-elias-alvarez/nvim-lsp-ts-utils",
+      "jose-elias-alvarez/null-ls.nvim",
+      "jay-babu/mason-null-ls.nvim",
+      "nvim-lsp-ts-utils",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = require("lsp-config"),
+  },
+  { "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "javascript",
+          "html",
+          "css",
+          "bash",
+          "lua",
+          "json",
+          "python",
+          "php",
+          "yaml",
+          "vim",
+          "help",
+          "rust",
+          "go"
+        },
+        highlight = { enable = true, }
+      }
+    end },
+  { "terrortylor/nvim-comment",
+    keys = {
+      { "<C-#>", "<CMD>CommentToggle<CR>j", mode = { "n" } },
+      { "<C-#>", "<C-\\><C-N><CMD>CommentToggle<CR>ji", mode = { "i" } },
+      { "<C-#>", ":'<,'>CommentToggle<CR>gv<esc>j", mode = { "v" } },
+    },
+    config = function()
+      require("nvim_comment").setup()
+    end },
+  { "fedepujol/move.nvim",
+    keys = {
+      { "<A-Down>", ":MoveLine(1)<CR>", mode = { "n" } },
+      { "<A-Up>", ":MoveLine(-1)<CR>", mode = { "n" } },
+      { "<A-Down>", ":MoveBlock(1)<CR>", mode = { "v" } },
+      { "<A-Up>", ":MoveBlock(-1)<CR>", mode = { "v" } },
+      { "<A-Down>", "<C-\\><C-N>:MoveLine(1)<CR>i", mode = { "i" } },
+      { "<A-Up>", "<C-\\><C-N>:MoveLine(-1)<CR>i", mode = { "i" } },
+    } },
+  { "lewis6991/gitsigns.nvim",
+    config = {
+      signs = {
+        add = { hl = "DiffAdd", text = "▌", numhl = "GitSignsAddNr" },
+        change = { hl = "DiffChange", text = "▌", numhl = "GitSignsChangeNr" },
+        delete = { hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr" },
+        topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
+        changedelete = { hl = "DiffChange", text = "~", numhl = "GitSignsChangeNr" }
+      },
+      numhl = false,
+      keymaps = {
+        -- Default keymap options
+        noremap = true,
+        buffer = true,
+        ["n >"] = { expr = true, '&diff ? \']c\' : \'<cmd>lua require"gitsigns".next_hunk()<CR>\'' },
+        ["n <"] = { expr = true, '&diff ? \'[c\' : \'<cmd>lua require"gitsigns".prev_hunk()<CR>\'' },
+        ["n <leader>bl"] = '<cmd>lua require"gitsigns".blame_line()<CR>'
+      },
+      watch_gitdir = {
+        interval = 100
+      },
+      sign_priority = 5,
+      status_formatter = nil -- Use default
+    },
+  },
+  { "akinsho/toggleterm.nvim",
+    -- config = { open_mapping = [[<leader>tt]], direction = "tab" }
+    config = true,
+    init = function()
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+    end,
+    keys = {
+      { "<Leader>lg", "<cmd>lua _lazygit_toggle()<CR>" },
+      { "<Leader>tt", "<Cmd>ToggleTermToggleAll<CR>" },
+      { "<Leader>th", "<Cmd>ToggleTerm direction=horizontal<CR>" },
+      { "<Leader>tv", "<Cmd>ToggleTerm direction=vertical size=40<CR>" }
+    }
+  },
+  { "lukas-reineke/indent-blankline.nvim",
+    config = {
+      char = "|",
+      buftype_exclude = { "terminal" },
+      filetype_exclude = { "help", "terminal", "dashboard", "mason" },
+      show_trailing_blankline_indent = false,
+      show_first_indent_level = false,
+    }
+
+  },
+  { "ggandor/leap.nvim",
+    config = function()
+      require("leap").set_default_keymaps()
+    end
+  },
+  { "famiu/bufdelete.nvim",
+  },
+  { "akinsho/nvim-bufferline.lua",
+    lazy = false,
+    config = {
+      options = {
+        offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+        buffer_close_icon = "",
+        modified_icon = "",
+        close_icon = "",
+        left_trunc_marker = "",
+        right_trunc_marker = "",
+        max_name_length = 14,
+        max_prefix_length = 13,
+        tab_size = 20,
+        show_tab_indicators = true,
+        enforce_regular_tabs = false,
+        view = "multiwindow",
+        show_buffer_close_icons = true,
+        separator_style = "thin",
+        diagnostics = "nvim_diagnostic",
+        numbers = "ordinal"
+      }
+    },
+    keys = {
+      { "bn", "<Cmd>tabnew<CR>" },
+      { "bq", "<Cmd>Bdelete<CR>" },
+      { "<TAB>", "<Cmd>BufferLineCycleNext<CR>" },
+      { "<S-TAB>", "<Cmd>BufferLineCyclePrev<CR>" },
+      { "<leader><right>", "<Cmd>BufferLineCycleNext<CR>" },
+      { "<leader><left>", "<Cmd>BufferLineCyclePrev<CR>" },
+      { "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>" },
+      { "<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>" },
+      { "<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>" },
+      { "<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>" },
+      { "<leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>" },
+      { "<leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>" },
+      { "<leader>7", "<Cmd>BufferLineGoToBuffer 7<CR>" },
+      { "<leader>8", "<Cmd>BufferLineGoToBuffer 8<CR>" },
+      { "<leader>9", "<Cmd>BufferLineGoToBuffer 9<CR>" },
+    }
+  },
+  { "norcalli/nvim-colorizer.lua", config = true, lazy = false },
+  { "windwp/nvim-autopairs", lazy = false, config = true },
+  { "alvan/vim-closetag", lazy = false },
+  { "907th/vim-auto-save", lazy = false },
+  { 'editorconfig/editorconfig-vim', lazy = false }
+}
---
+++ lua/lsp-config.lua
@@ -0,0 +1,148 @@
+local config = function()
+  require('mason').setup()
+
+  local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+      options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  end
+
+  -- local handlers = {
+  --   -- ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover),
+  --   -- ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help),
+  --   ['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  --     virtual_text = true,
+  --     signs = true,
+  --     underline = true,
+  --     update_in_insert = false,
+  --
+  --   })
+  -- }
+
+  -- format on save
+  vim.cmd([[augroup fmt
+    autocmd!
+    autocmd BufWritePre * undojoin | lua vim.lsp.buf.format({async = false})
+  augroup END]])
+
+  -- replace the default lsp diagnostic letters with prettier symbols
+  --
+  vim.fn.sign_define("DiagnosticSignError", { text = "✗", texthl = "DiagnosticSignError" })
+  vim.fn.sign_define("DiagnosticSignWarn", { text = "!", texthl = "DiagnosticSignWarn" })
+  vim.fn.sign_define("DiagnosticSignInformation", { text = "", texthl = "DiagnosticSignInfo" })
+  vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+
+  local opts = { noremap = true, silent = true }
+
+  local function on_attach(client, bufnr)
+    map('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    map('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts)
+    map('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    -- map('n', 'gI', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    map('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    map('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    map('n', '<space>D', '<cmd>Telescope lsp_type_definitions<CR><CR>', opts)
+    map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    map('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts)
+    map('n', 'gE', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    map('n', 'ge', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    map('n', '<space>e', '<cmd>lua require\'telescope.builtin\'.diagnostics({bufnr =  0, previewer = false})<CR>',
+      opts)
+    map('n', '<space>E', '<cmd>lua require\'telescope.builtin\'.diagnostics({previewer = false})<CR>', opts)
+
+    -- Set some keybinds conditional on server capabilities
+    if client.server_capabilities.documentFormattingProvider then
+      map("n", "<space>fm", "<cmd>lua vim.lsp.buf.format({async = true})<CR>", opts)
+    elseif client.server_capabilities.documentRangeFormattingProvider then
+      map("n", "<space>fm", "<cmd>lua vim.lsp.buf.range({async = true})<CR>", opts)
+    end
+  end
+
+  local null_ls = require("null-ls")
+  null_ls.setup({
+    on_attach = on_attach,
+    root_dir = function()
+      return vim.loop.cwd()
+    end,
+    sources = {
+      null_ls.builtins.diagnostics.eslint_d, -- eslint or eslint_d
+      null_ls.builtins.code_actions.eslint_d, -- eslint or eslint_d
+      null_ls.builtins.formatting.prettierd -- prettier, eslint, eslint_d, or prettierd
+    },
+  })
+
+  local mason_lspconfig = require("mason-lspconfig")
+  local lspconfig = require 'lspconfig'
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+  mason_lspconfig.setup_handlers({
+    function(server_name) -- default handler (optional)
+      require("lspconfig")[server_name].setup({
+        root_dir = function()
+          return vim.loop.cwd()
+        end,
+        -- handlers = handlers,
+        on_attach = on_attach,
+        capabilities = capabilities
+      })
+    end,
+    ["tsserver"] = function()
+      lspconfig.tsserver.setup({
+        init_options = require("nvim-lsp-ts-utils").init_options,
+        capabilities = capabilities,
+        on_attach = function(client)
+          local ts_utils = require("nvim-lsp-ts-utils")
+          ts_utils.setup({
+            update_imports_on_move = true,
+            require_confirmation_on_move = true,
+            auto_inlay_hints = true,
+            inlay_hints_highlight = "Comment",
+            -- eslint_bin = "eslint_d",
+            eslint_enable_diagnostics = false,
+            eslint_enable_code_actions = true,
+            enable_formatting = false,
+            -- formatter = "eslint_d",
+          })
+          ts_utils.setup_client(client)
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+          map("n", "gs", ":TSLspOrganize<CR>", opts)
+          map("n", "<space>rnf", ":TSLspRenameFile<CR>", opts)
+          map("n", "go", ":TSLspImportAll<CR>", opts)
+        end
+      })
+
+    end,
+    ["sumneko_lua"] = function()
+      lspconfig.sumneko_lua.setup {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }
+            },
+            workspace = {
+              library = {
+                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+              }
+            },
+            telemetry = {
+              enable = false
+            }
+          }
+
+        }
+      }
+    end,
+
+  })
+
+  vim.diagnostic.config { virtual_text = true }
+end
+
+return config
