8646bdfe8de5e874a775ea4feb3dfa8831645240
Florian Krause <florian.krause@etribes.de>
2024-05-23 17:33:43 +0200

    Try Neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.scrollback = 1000
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.o.guifont = "FiraCode NFM:h17" -- text below applies for VimScript
+end
