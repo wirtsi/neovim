b210edb00960d8597e3d5f0d025d5076ec2b2b9d
Florian Krause <florian.krause@etribes.de>
2024-07-05 13:33:47 +0200

    neovide setup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,5 +4,17 @@
 vim.g.scrollback = 1000
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
-  vim.o.guifont = "FiraCode NFM:h17" -- text below applies for VimScript
+  vim.g.neovide_cursor_animation_length = 0.03
+  vim.g.neovide_cursor_trail_size = 0.75
+  vim.g.neovide_cursor_vfx_mode = "ripple"
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_remember_window_position = true
+  vim.g.neovide_cursor_antialiasing = true
+  vim.o.guifont = "FiraCode NFM:h16" -- text below applies for VimScript
+  vim.g.neovide_fullscreen = true
+  -- Allow clipboard copy paste in neovim
+  vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 end
