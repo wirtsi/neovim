0977eefbde303091b94f776dfbb87afc869647bf
Florian Krause <florian.krause@etribes.de>
2025-01-06 09:29:31 +0100

    Update to Snacks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.scrollback = 1000
+-- views can only be fully collapsed with the global statusline
+vim.opt.laststatus = 3
+-- Default splitting will cause your main splits to jump when opening an edgebar.
+-- To prevent this, set `splitkeep` to either `screen` or `topline`.
+vim.opt.splitkeep = "screen"
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   vim.g.neovide_cursor_animation_length = 0.03
--- lua/plugins/mine.lua
+++ lua/plugins/mine.lua
@@ -10,6 +10,21 @@
 
 local Util = require("lazyvim.util")
 
+-- local terminal_group = vim.api.nvim_create_augroup("TerminalGroup", { clear = true })
+--
+-- -- Define autocommands for the terminal
+-- vim.api.nvim_create_autocmd("TermOpen", {
+--   group = terminal_group,
+--   pattern = "term://*",
+--   callback = function()
+--     -- Configure the terminal settings
+--     vim.opt_local.number = false
+--     vim.opt_local.relativenumber = false
+--     -- vim.cmd("set nobuflisted")
+--     vim.cmd("startinsert")
+--   end,
+-- })
+
 return {
   {
     "simrat39/symbols-outline.nvim",
@@ -68,5 +83,90 @@ return {
         end,
       })
     end,
+    {
+      "olimorris/codecompanion.nvim",
+      lazy = false,
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "hrsh7th/nvim-cmp",
+        "nvim-telescope/telescope.nvim",
+      },
+      config = function()
+        require("codecompanion").setup({
+          adapters = {
+            llama3 = function()
+              return require("codecompanion.adapters").extend("ollama", {
+                name = "llama3", -- Give this adapter a different name to differentiate it from the default ollama adapter
+                schema = {
+                  model = {
+                    default = "qwen2.5-coder:7b",
+                  },
+                  num_ctx = {
+                    default = 4096,
+                  },
+                  num_predict = {
+                    default = -1,
+                  },
+                },
+              })
+            end,
+          },
+          strategies = {
+            chat = {
+              adapter = "llama3",
+            },
+            inline = {
+              adapter = "llama3",
+            },
+            agent = {
+              adapter = "llama3",
+            },
+          },
+          display = {
+            chat = {
+              window = {
+                layout = "vertical", -- float|vertical|horizontal|buffer
+              },
+            },
+          },
+          -- opts = {
+          --   ---@param adapter CodeCompanion.Adapter
+          --   ---@return string
+          --   system_prompt = function(adapter)
+          --     if adapter.schema.model.default == "llama3.1:latest" then
+          --       return "My custom system prompt"
+          --     end
+          --     return "My default system prompt"
+          --   end,
+          -- },
+        })
+      end,
+      init = function() end,
+    },
+  },
+
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      open_mapping = [[<c-y>]],
+      insert_mapping = true,
+      terminal_mapping = true,
+      direction = "vertical",
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.2
+        end
+      end,
+    },
+  },
+  {
+    "wnkz/monoglow.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
   },
 }
