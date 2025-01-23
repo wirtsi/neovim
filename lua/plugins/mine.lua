94fa32b46975f7536a2892a53002f296e1339bf7
Florian Krause <florian.krause@etribes.de>
2025-01-23 11:44:20 +0100

    Almost working terminal setup

--- lua/plugins/mine.lua
+++ lua/plugins/mine.lua
@@ -83,90 +83,133 @@ return {
         end,
       })
     end,
-    {
-      "olimorris/codecompanion.nvim",
-      lazy = false,
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "hrsh7th/nvim-cmp",
-        "nvim-telescope/telescope.nvim",
+  },
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>ue",
+        function()
+          require("edgy").toggle()
+        end,
+        desc = "Edgy Toggle",
       },
-      config = function()
-        require("codecompanion").setup({
-          adapters = {
-            llama3 = function()
-              return require("codecompanion.adapters").extend("ollama", {
-                name = "llama3", -- Give this adapter a different name to differentiate it from the default ollama adapter
-                schema = {
-                  model = {
-                    default = "qwen2.5-coder:7b",
-                  },
-                  num_ctx = {
-                    default = 4096,
-                  },
-                  num_predict = {
-                    default = -1,
-                  },
-                },
-              })
+    -- stylua: ignore
+    { "<leader>uE", function() require("edgy").select() end, desc = "Edgy Select Window" },
+    },
+    opts = function()
+      local opts = {
+        bottom = {
+          {
+            ft = "toggleterm",
+            size = { height = 0.4 },
+            filter = function(buf, win)
+              return vim.api.nvim_win_get_config(win).relative == ""
             end,
           },
-          strategies = {
-            chat = {
-              adapter = "llama3",
-            },
-            inline = {
-              adapter = "llama3",
-            },
-            agent = {
-              adapter = "llama3",
-            },
+          {
+            ft = "noice",
+            size = { height = 0.4 },
+            filter = function(buf, win)
+              return vim.api.nvim_win_get_config(win).relative == ""
+            end,
           },
-          display = {
-            chat = {
-              window = {
-                layout = "vertical", -- float|vertical|horizontal|buffer
-              },
-            },
+          "Trouble",
+          { ft = "qf", title = "QuickFix" },
+          {
+            ft = "help",
+            size = { height = 20 },
+            -- don't open help files in edgy that we're editing
+            filter = function(buf)
+              return vim.bo[buf].buftype == "help"
+            end,
           },
-          -- opts = {
-          --   ---@param adapter CodeCompanion.Adapter
-          --   ---@return string
-          --   system_prompt = function(adapter)
-          --     if adapter.schema.model.default == "llama3.1:latest" then
-          --       return "My custom system prompt"
-          --     end
-          --     return "My default system prompt"
-          --   end,
-          -- },
-        })
-      end,
-      init = function() end,
-    },
-  },
+          { title = "Spectre", ft = "spectre_panel", size = { height = 0.4 } },
+          { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
+        },
+        left = {
+          { title = "Neotest Summary", ft = "neotest-summary" },
+          -- "neo-tree",
+        },
+        right = {
+          { title = "Grug Far", ft = "grug-far", size = { width = 0.4 } },
+        },
+        keys = {
+          -- increase width
+          ["<c-Right>"] = function(win)
+            win:resize("width", 2)
+          end,
+          -- decrease width
+          ["<c-Left>"] = function(win)
+            win:resize("width", -2)
+          end,
+          -- increase height
+          ["<c-Up>"] = function(win)
+            win:resize("height", 2)
+          end,
+          -- decrease height
+          ["<c-Down>"] = function(win)
+            win:resize("height", -2)
+          end,
+        },
+      }
 
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    opts = {
-      open_mapping = [[<c-y>]],
-      insert_mapping = true,
-      terminal_mapping = true,
-      direction = "vertical",
-      size = function(term)
-        if term.direction == "horizontal" then
-          return 15
-        elseif term.direction == "vertical" then
-          return vim.o.columns * 0.2
+      if LazyVim.has("neo-tree.nvim") then
+        local pos = {
+          filesystem = "left",
+          buffers = "top",
+          git_status = "right",
+          document_symbols = "bottom",
+          diagnostics = "bottom",
+        }
+        local sources = LazyVim.opts("neo-tree.nvim").sources or {}
+        for i, v in ipairs(sources) do
+          table.insert(opts.left, i, {
+            title = "Neo-Tree " .. v:gsub("_", " "):gsub("^%l", string.upper),
+            ft = "neo-tree",
+            filter = function(buf)
+              return vim.b[buf].neo_tree_source == v
+            end,
+            pinned = true,
+            open = function()
+              vim.cmd(("Neotree show position=%s %s dir=%s"):format(pos[v] or "bottom", v, LazyVim.root()))
+            end,
+          })
         end
-      end,
-    },
-  },
-  {
-    "wnkz/monoglow.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
+      end
+
+      -- trouble
+      for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+        opts[pos] = opts[pos] or {}
+        table.insert(opts[pos], {
+          ft = "trouble",
+          filter = function(_buf, win)
+            return vim.w[win].trouble
+              and vim.w[win].trouble.position == pos
+              and vim.w[win].trouble.type == "split"
+              and vim.w[win].trouble.relative == "editor"
+              and not vim.w[win].trouble_preview
+          end,
+        })
+      end
+
+      -- snacks terminal
+      for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+        opts[pos] = opts[pos] or {}
+        table.insert(opts[pos], {
+          ft = "snacks_terminal",
+          size = { height = 0.4 },
+          title = "%{b:snacks_terminal.id}: %{b:term_title}",
+          filter = function(_buf, win)
+            return vim.w[win].snacks_win
+              and vim.w[win].snacks_win.position == pos
+              and vim.w[win].snacks_win.relative == "editor"
+              and not vim.w[win].trouble_preview
+          end,
+        })
+      end
+      return opts
+    end,
   },
 }
