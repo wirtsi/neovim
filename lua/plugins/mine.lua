c6b69a70e940897fcfc05e66bb7f89c0be32c761
Florian Krause <florian.krause@etribes.de>
2025-09-17 18:05:06 +0200

    Bump versions

--- lua/plugins/mine.lua
+++ lua/plugins/mine.lua
@@ -25,7 +25,6 @@ local Util = require("lazyvim.util")
 --   end,
 -- })
 --
-local _ = require("../config/avante")
 
 return {
   {
@@ -65,53 +64,19 @@ return {
     "rcarriga/nvim-notify",
     enabled = false,
   },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    opts = {
-      -- add any opts here
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",        -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  }
+  -- {
+  --   "ramilito/kubectl.nvim",
+  --   -- use a release tag to download pre-built binaries
+  --   version = "2.*",
+  --   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  --   -- build = 'cargo build --release',
+  --   dependencies = "saghen/blink.download",
+  --   config = function()
+  --     require("kubectl").setup()
+  --   end,
+  --   keys = {
+  --     { "<leader>k", '<cmd>lua require("kubectl").toggle()<cr>' },
+  --     { "7", "<Plug>(kubectl.view_nodes)", ft = "k8s_*" },
+  --   },
+  -- },
 }
